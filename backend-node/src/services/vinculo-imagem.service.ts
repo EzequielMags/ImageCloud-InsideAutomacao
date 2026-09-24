eba500e4684f5d839166c55eb4c63d18974c90fa
Ezequiel Magoga <ezequielmagoga2005@gmail.com>
2026-09-24 15:47:14 -0300

    feat: remove unnecessary import

--- backend-node/src/services/vinculo-imagem.service.ts
+++ backend-node/src/services/vinculo-imagem.service.ts
@@ -1,4 +1,3 @@
-import { stat } from "fs"// tentar excluir dps (n quero estragar os commitss)
 import ImagensRepository from "../repository/imagens.respository.js"
 import { Produto, ProdutosRepository } from "../repository/produtos.repository.js"
 import TextService from "./text.service.js"
